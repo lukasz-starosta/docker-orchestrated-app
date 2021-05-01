47841381ee5194c74727c356a20636f452d02df3
lukasz-starosta <lstarosta98@gmail.com>
2021-05-01 17:05:00 +0200

    Fix db

--- products/config/knexfile.js
+++ products/config/knexfile.js
@@ -1,6 +1,8 @@
 module.exports = {
     client: 'mysql',
     connection: {
+        host: 'products-database',
+        port: '3306',
         user: 'root',
         password: 'password',
         database: 'products',
